e7d2a33d86b2e6ccf1f54a3f50b34dff3f2768bc
PrzemekZiemiakowski <przemo1814@gmail.com>
2022-05-22 16:56:36 +0200

    added sql connection and cursor to main/ending work till some day

--- main.py
+++ main.py
@@ -10,24 +10,16 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    conn = pyodbc.connect('Driver={SQL Server};'
-                          'Server=DESKTOP-7S9CDR1\SQLEXPRESS;'
-                          'Database=database_name;'
-                          'Trusted_Connection=yes;')
-    conn.cursor()
 
     for x in range(1, 50):
+        conn = pyodbc.connect('Driver={SQL Server};'
+                              'Server=DESKTOP-7S9CDR1\SQLEXPRESS;'
+                              'Database=database_name;'
+                              'Trusted_Connection=yes;')
+        conn.cursor()
 
         generator.generateSprzet()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-import pymssql
-    conn = pymssql.connect(server='yourserver.database.windows.net', user='yourusername@yourserver', password='yourpassword', database='AdventureWorks')
-    cursor = conn.cursor()
-    cursor.execute("INSERT SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, SellStartDate) OUTPUT INSERTED.ProductID VALUES ('SQL Server Express', 'SQLEXPRESS', 0, 0, CURRENT_TIMESTAMP)")
-    row = cursor.fetchone()
-    while row:
-        print "Inserted Product ID : " +str(row[0])
-        row = cursor.fetchone()
-    conn.commit()
-    conn.close()
+        row = conn.fetchone()
+        while row:
+            print(row[0])
+            row = conn.fetchone()
